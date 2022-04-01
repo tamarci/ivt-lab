0eaced414dd104dc90ec2ef43c8bd04c9ddccbc1
tamarci <tarnokmr@gmail.com>
2022-04-01 13:14:58 +0200

    hehe

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -40,7 +40,7 @@ public class GT4500 implements SpaceShip {
 
     switch (firingMode) {
       case SINGLE:
-        if (wasPrimaryFiredLast) {
+        if ( !wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
             firingSuccess = secondaryTorpedoStore.fire(1);
